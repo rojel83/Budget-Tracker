879b981f84b3acd562809c1b44698b5082178543
Rojel Perez Vez <rojelpv@gmail.com>
2020-05-09 09:41:56 -0400

    cheking if the app is online  and  setting up to delet records if sccessful

--- public/db.js
+++ public/db.js
@@ -9,3 +9,53 @@ request.onupgradeneeded = function (event) {
 request.onsuccess = function (event) {
     db = event.target.result;
 
+    // check if app is online before reading from db
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("Woops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    // delete records if successful
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+function deletePending() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.clear();
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
